08d12aecaed41423276c1059a90b89d8e8dc13d1
nijimajohn <hirokikotera0211@gmail.com>
2021-08-19 06:20:57 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index,:edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "削除完了"
+    redirect_to users_url
+  end
+  
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
+  
   def new
     @user = User.new
   end
@@ -18,11 +34,53 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+    
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  　end
+  　
+  　def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+    end
+    
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+    
   
 end
